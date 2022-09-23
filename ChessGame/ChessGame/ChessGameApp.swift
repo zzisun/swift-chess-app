8cc72e95d16f85f851ecb67ab9f2a9806c650cb6
Sunghyun Kim <sunghyun_k@icloud.com>
2022-09-24 00:24:27 +0900

    프로젝트 생성

---
+++ ChessGame/ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by Sunghyun Kim on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
