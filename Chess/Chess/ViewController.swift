0e118e50a595d6d1bdd595d0a391e829d98de9b3
JAEYOUNGYUN <lastmore9@gmail.com>
2022-09-23 23:31:05 +0900

    Init project

---
+++ Chess/Chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by jaeyoung Yun on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
