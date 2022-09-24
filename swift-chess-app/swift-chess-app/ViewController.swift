23595aaf3494118b942e3949b40ad8e30ec5e8f0
haeseoklee <haeseoklee.dev@gmail.com>
2022-09-24 20:20:23 +0900

    feat: init project

---
+++ swift-chess-app/swift-chess-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-chess-app
+//
+//  Created by Haeseok Lee on 2022/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
