7047d3da775cb1d1eda1efa603a6e22ef5a494fc
zzisun <jisunkim98@gmail.com>
2022-09-27 00:34:59 +0900

    feat: create project

---
+++ ChessApp/ChessApp/ChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppApp.swift
+//  ChessApp
+//
+//  Created by 김지선 on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct ChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessApp/ChessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessApp
+//
+//  Created by 김지선 on 2022/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
