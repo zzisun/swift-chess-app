0d4c746719254d31da66aa40e95a361ca8c71421
zzisun <jisunkim98@gmail.com>
2022-09-28 01:15:57 +0900

    feat: test할 case의 mock 선언

--- ChessApp/ChessAppTests/MockPlayer.swift
+++ ChessApp/ChessAppTests/MockPlayer.swift
@@ -8,16 +8,36 @@
 import Foundation
 @testable import ChessApp
 
-final class MockPlayer: Playerable {
-    let type: PlayerType
+final class MockPlayerA: Playerable {
+    let type: PlayerType = .black
+    var moveCallCount = 0
+    let current = Coordinates(file: .A, rank: .two)
+    let next = Coordinates(file: .A, rank: .three)
+
+    func move() -> (current: Coordinates, next: Coordinates) {
+        moveCallCount += 1
+        return (current, next)
+    }
+}
+
+final class MockPlayerB: Playerable {
+    let type: PlayerType = .black
+    var moveCallCount = 0
+    let current = Coordinates(file: .A, rank: .two)
+    let next = Coordinates(file: .B, rank: .two)
+    
+    func move() -> (current: Coordinates, next: Coordinates) {
+        moveCallCount += 1
+        return (current, next)
+    }
+}
+
+final class MockPlayerC: Playerable {
+    let type: PlayerType = .black
     var moveCallCount = 0
     let current = Coordinates(file: .A, rank: .two)
     let next = Coordinates(file: .A, rank: .three)
     
-    init(type: PlayerType) {
-        self.type = type
-    }
-
     func move() -> (current: Coordinates, next: Coordinates) {
         moveCallCount += 1
         return (current, next)
