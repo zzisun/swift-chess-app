097a314d8e59a69046aeacefec1ea8fc20bd02c4
zzisun <jisunkim98@gmail.com>
2022-09-28 01:05:06 +0900

    feat: test for moving pawn

--- ChessApp/ChessApp/Model/Player.swift
+++ ChessApp/ChessApp/Model/Player.swift
@@ -11,31 +11,3 @@ protocol Playerable {
     var type: PlayerType { get }
     func move() -> (current: Coordinates, next: Coordinates)
 }
-
-final class MockPlayer: Playerable {
-    let type: PlayerType
-    var moveCallCount = 0
-    
-    init(type: PlayerType) {
-        self.type = type
-    }
-
-    func move() -> (current: Coordinates, next: Coordinates) {
-        let current = Coordinates(file: .A, rank: .two)
-        let next = Coordinates(file: .A, rank: .three)
-        return (current, next)
-    }
-}
-
-//final class Player: Playerable {
-//    let type: PlayerType
-//
-//    init(type: PlayerType) {
-//        self.type = type
-//    }
-//
-//    func move() -> (Coordinates, Coordinates) {
-//        return (current, next)
-//    }
-//
-//}
