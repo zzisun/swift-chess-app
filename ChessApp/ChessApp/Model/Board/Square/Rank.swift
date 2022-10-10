0379f6efb6ccebc491706797ae1f2bcb8dad3fce
zzisun <jisunkim98@gmail.com>
2022-10-10 14:12:19 +0900

    refactor: propertywrapper로 custom int 구현

--- ChessApp/ChessApp/Model/Board/Square/Rank.swift
+++ ChessApp/ChessApp/Model/Board/Square/Rank.swift
@@ -1,15 +1,44 @@
+//
+//  Rank.swift
+//  ChessApp
+//
+//  Created by 김지선 on 2022/10/10.
+//
+
 import Foundation
 
-struct Rank { // Rank 자체를 비교하고 계산할 수 있다면 좋겠다. 그러면 Square자체에서 해결가능
-  let value: Int
-  private var range = 0..<Constants.length
+@propertyWrapper
+struct Rank { // propertyWrapper error throws 어디서 해줘야 좋을까?
+  private var value: Int
+  private let range = 1...Constants.length
   
   init(value: Character) throws {
     if let intValue = value.wholeNumberValue,
-       range ~= intValue - 1 {
-      self.value = intValue - 1
+       range ~= intValue {
+      self.value = intValue
     } else {
       throw ChessError.invalidRank
     }
+  }
+  
+  var wrappedValue: Int {
+    get { return value }
+    set { value = value - 1 }
+  }
+}
+
+@propertyWrapper
+struct MaxPriceOrLessWrapper {
+  private var max: Int
+  private var value: Int
+  
+  init(value: Int, maxPrice: Int) {
+    self.max = maxPrice
+    self.value = min(value, maxPrice)
+  }
+  
+  var wrappedValue: Int {
+    get { return value }
+    set { value = min(newValue,max) }
   }
 }
