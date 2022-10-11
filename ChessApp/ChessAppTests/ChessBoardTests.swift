4f143418b60ff3fdfe0a89163310483a427bddf7
zzisun <jisunkim98@gmail.com>
2022-10-11 11:41:55 +0900

    feat: chess piece 테스트

--- ChessApp/ChessAppTests/ChessBoardTests.swift
+++ ChessApp/ChessAppTests/ChessBoardTests.swift
@@ -9,6 +9,19 @@
 //@testable import ChessApp
 //
 //final class ChessBoardTests: XCTestCase {
+//func test_Square_Difference() throws {
+//  let fileA = try File("A")
+//  let fileB = try File("B")
+//  let rank1 = try Rank("1")
+//  let rank2 = try Rank("2")
+//  let A1 = Square(file: fileA, rank: rank1)
+//  let B2 = Square(file: fileB, rank: rank2)
+//  let expected = (file: 1, rank: 1)
+//
+//  let difference = B2 - A1
+//  XCTAssertEqual(difference.file, expected.file)
+//  XCTAssertEqual(difference.rank, expected.rank)
+//}
 //
 //    override func setUpWithError() throws {
 //        // Put setup code here. This method is called before the invocation of each test method in the class.
---
+++ ChessApp/ChessAppTests/ChessPieceTests.swift
@@ -0,0 +1,13 @@
+//
+//  ChessPieceTests.swift
+//  ChessAppTests
+//
+//  Created by 김지선 on 2022/10/10.
+//
+
+import XCTest
+@testable import ChessApp
+
+final class ChessPieceTests: XCTestCase {
+  
+}
